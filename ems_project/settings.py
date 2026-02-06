2c98807b6da989ac14e6f41b32035c94f9a41a4c
pavinnivap <pavin4041@gmail.com>
2026-02-06 15:57:58 +0530

    cors allowed methods and headers

--- ems_project/settings.py
+++ ems_project/settings.py
@@ -161,7 +161,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    
     "http://localhost:5173",
 ]
 
@@ -185,5 +184,22 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
 
 
