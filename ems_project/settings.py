a919e5cd38ac3eef2d8553df6538b55042993160
pavinnivap <pavin4041@gmail.com>
2026-02-06 12:13:07 +0530

    Configured PostgreSql Database

--- ems_project/settings.py
+++ ems_project/settings.py
@@ -13,6 +13,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,10 +88,9 @@ WSGI_APPLICATION = 'ems_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
