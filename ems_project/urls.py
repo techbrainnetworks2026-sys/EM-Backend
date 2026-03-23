ea009e323b3e9494f8977b9495958e42323d0636
jayamoorthi9361-h <jayamoorthi9361@gmail.com>
2026-03-23 13:49:26 +0530

    change urls and add payroll management app

--- ems_project/urls.py
+++ ems_project/urls.py
@@ -18,14 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+'''
 from django.http import HttpResponse
 
 def health_check(request):
     return HttpResponse("OK")
-
+'''
 urlpatterns = [
-    path('', health_check),
+  #  path('', health_check),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/leave/', include('leave.urls')),
